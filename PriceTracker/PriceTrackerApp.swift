ead3b98f3ef5423d61ec723cc2f7e8dd7ff5bb6d
georgievb <georgievb@gmail.com>
2025-11-29 09:23:58 +0200

    Initial Commit

---
+++ PriceTracker/PriceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PriceTrackerApp.swift
+//  PriceTracker
+//
+//  Created by Boris Georgiev on 29.11.25.
+//
+
+import SwiftUI
+
+@main
+struct PriceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PriceTrackerTests/PriceTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PriceTrackerTests.swift
+//  PriceTrackerTests
+//
+//  Created by Boris Georgiev on 29.11.25.
+//
+
+import Testing
+@testable import PriceTracker
+
+struct PriceTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
