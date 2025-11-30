fa87d004f8dd7d3bfd60af58f9be1237312fc183
georgievb <georgievb@gmail.com>
2025-11-30 21:39:17 +0200

    Added deep link handling using stocks://symbol/XXXX scheme. 	- The NavigationStack now uses NavigationPath so navigation can be triggered programatically.

--- PriceTracker/PriceTrackerApp.swift
+++ PriceTracker/PriceTrackerApp.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 @main
 struct PriceTrackerApp: App {
+    @StateObject private var feedViewModel = FeedViewModel(service: EchoPriceService())
+    @State private(set) var path = NavigationPath()
     
     var body: some Scene {
         WindowGroup {
-            NavigationStack {
-                FeedView(viewModel: FeedViewModel(service: EchoPriceService()))
+            NavigationStack(path: $path) {
+                FeedView(viewModel: feedViewModel)
+                    .onOpenURL { url in
+                        handleDeepLink(url)
+                    }
             }
         }
     }
     
+    private func handleDeepLink(_ url: URL) {
+        guard url.scheme == "stocks",
+              url.host == "symbol",
+              let symbol = url.pathComponents.dropFirst().first?.uppercased() else {
+            return
+        }
+
+        if let priceData = feedViewModel.data.first(where: { $0.symbol == symbol }) {
+            path = NavigationPath()
+            Task { @MainActor in
+                try? await Task.sleep(nanoseconds: 300_000_000)
+                path.append(priceData)
+            }
+        }
+    }
 }
